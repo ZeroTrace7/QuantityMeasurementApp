895cd3e419d9418449336c367dd718521d7edb94
ZeroTrace7 <shreyashgupta999@gmail.com>
2026-04-17 09:52:59 +0530

    Base application setup

---
+++ src/main/java/com/zerotrace7/quantitymeasurement/QuantityMeasurementApp.java
@@ -0,0 +1,7 @@
+package com.zerotrace7.quantitymeasurement;
+
+public class QuantityMeasurementApp {
+    public static void main(String[] args) {
+        System.out.println("Quantity Measurement Application");
+    }
+}
